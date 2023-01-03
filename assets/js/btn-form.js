dedcdb9e7aac2f29fe722768826663cd74920cb3
Daniel Moraes <96148567+SirDnx@users.noreply.github.com>
2023-01-02 23:29:34 -0300

    formulario de recuperar conta adicionado e alteracoes no video de fundo

--- assets/js/btn-form.js
+++ assets/js/btn-form.js
@@ -1,10 +1,13 @@
 const btnCadastrar = document.querySelector('#btnCadastrar');
 const btnLogar = document.querySelector('#btnLogar');
+const btnRecuperar = document.querySelector('#btnRecuperar');
 const formLogin = document.querySelector('#form-login');
 const formCadastro = document.querySelector('#formCadastro');
+const formRecuperar = document.querySelector('#formRecuperar');
 
 btnCadastrar.addEventListener("click", function(){
     btnCadastrar.style.display = 'none';
+    btnRecuperar.style.display = 'none';
     btnLogar.style.display = 'block';  
     formLogin.style.display = 'none'; 
     formCadastro.style.display = 'grid';  
@@ -12,7 +15,17 @@ btnCadastrar.addEventListener("click", function(){
 
 btnLogar.addEventListener("click", function(){
     btnCadastrar.style.display = 'block';
+    btnRecuperar.style.display = 'block';
     btnLogar.style.display = 'none';
     formLogin.style.display = 'grid';
     formCadastro.style.display = 'none';  
+    formRecuperar.style.display = 'none';
 });
+
+btnRecuperar.addEventListener("click", function(){
+    btnCadastrar.style.display = 'none';
+    btnRecuperar.style.display = 'none';
+    btnLogar.style.display = 'block';
+    formLogin.style.display = 'none';
+    formRecuperar.style.display = 'grid';  
+})
